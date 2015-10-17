6660c087109fa9e3287735faa354ca8ddb79edd1
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-17 16:45:24 +0200

    add moar configs

--- lib/cartbotsy/config.rb
+++ lib/cartbotsy/config.rb
@@ -1,7 +1,8 @@
 module Cartbotsy
   module Config
-    API_TOKEN = ''
-    WEBHOOK_TOKEN = ''
+    API_TOKEN = 'xoxb-12641009472-DUwv0nlHVccxBpoAqYr8W3Go'
+    WEBHOOK_TOKEN = 'TE6oBjT6V6OTqtNNOckRGiq1'
+    APP_URL = "http://localhost:3000"
     VERSION = '0.0.1'
     AVAILABLE_CITIES = [
       'gdansk',
