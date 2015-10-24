d572f5fa257ad54a58f25c60b776eb911104685b
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-24 20:15:11 +0200

    add default values

--- lib/cartbotsy/configuration.rb
+++ lib/cartbotsy/configuration.rb
@@ -2,7 +2,7 @@ module Cartbotsy
   class Configuration
     attr_accessor :api_token, :webhook_token, :app_url, :available_cities
 
-    def initialize(api_token, webhook_token, app_url, available_cities)
+    def initialize(api_token = '', webhook_token = '', app_url = '', available_cities = [])
       @api_token = api_token
       @webhook_token = webhook_token
       @app_url = app_url
