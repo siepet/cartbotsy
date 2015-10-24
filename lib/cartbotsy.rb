1e594af1e01c6910e9b84096b0558c3284f7aef8
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2015-10-24 19:00:55 +0200

    add configuration

--- lib/cartbotsy.rb
+++ lib/cartbotsy.rb
@@ -1,8 +1,23 @@
 require "socket"
 require "./cartbotsy/api"
+require "./cartbotsy/configuration"
 require "./cartbotsy/config"
 require "./cartbotsy/parser"
 require "./cartbotsy/worker"
 require "pry"
+
+module Cartbotsy
+  class << self
+    attr_accessor :config
+
+    def config
+      @config ||= Configuration.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
+end
 
 Cartbotsy::Worker.start
---
+++ lib/cartbotsy/configuration.rb
@@ -0,0 +1,13 @@
+module Cartbotsy
+  class Configuration
+    attr_accessor :api_token, :webhook_token, :app_url, :available_cities
+
+    def initialize(api_token, webhook_token, app_url, available_cities)
+      @api_token = api_token
+      @webhook_token = webhook_token
+      @app_url = app_url
+      @available_cities = available_cities
+    end
+  end
+end
+
